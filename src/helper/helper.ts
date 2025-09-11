ce2bcd8d16974a9966fef63eca007e03a3cdaa09
innowinds-asset-management <innowinds_asset_management@hotmail.com>
2025-09-11 15:29:48 +0530

    Minor changes

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -14,3 +14,15 @@ export function generateEntityId(entityName: string) {
 }
 
 
+export function getTimestampString() {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hour = String(now.getHours()).padStart(2, "0");
+  const minute = String(now.getMinutes()).padStart(2, "0");
+  const second = String(now.getSeconds()).padStart(2, "0");
+
+  return `${year}${month}${day}${hour}${minute}${second}`;
+}
